b247a40186dfc0112e751387ddf60ea3e0816c61
Jonah Patinkin <jonahpatinkin@gmail.com>
2016-11-13 01:40:50 -0600

    finished pp-08

---
+++ pp-08/site.js
@@ -0,0 +1,15 @@
+$.noConflict();
+(function(){
+$(document).ready(
+  function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
+
+    if (typeof(window.$) !== "undefined") {
+      console.log('jQuery still controls the $ variable');
+    } else {
+      console.log('Nice work! jQuery no longer controls the $ variable');
+    }
+
+  }
+})(jQuery);
